f16de58c601d8f6e892f77290e157156edf9a06e
kimanjea <kimanjea@spu.edu>
2023-01-17 12:34:15 -0800

    Hymm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+ double addfunc (int n, int v) {
+     return n+v;
+ }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num;
+    int num2;
+  //  std::cout << "Hello, World!" << std::endl;
+
+    string food;
+    cout << "Enter a food name" << endl;
+    cin >> food;
+
+    cout << "enter food number ";
+    cin >>num;
+    cin >>num2;
+    num = addfunc(num,num2 );
+
     //let s got
+    cout << food;
+    cout << " you food adds up to =  " << num << endl;
     return 0;
 }
